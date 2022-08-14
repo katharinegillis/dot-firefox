5ef2f10dae763fad08b951b02a739e77b50ca062
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-13 23:49:54 -0600

    refactor: remove non-Linux logic from package

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
-
-if [ "$SYSTEM" == "wsl" ]; then
-    choco.exe install firefox --params "/NoTaskbarShortcut /NoDesktopShortcut /RemoveDistributionDir" -y
-elif [ "$SYSTEM" == "mac" ]; then
-    brew install --cask firefox
-elif [ "$SYSTEM" == "linux" ]; then
-    sudo apt install firefox
-fi
+sudo apt install firefox
--- update.sh
+++ update.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
-
-if [ "$SYSTEM" == "wsl" ]; then
-    echo "Please upgrade via Firefox instead of Chocolatey."
-elif [ "$SYSTEM" == "mac" ]; then
-    brew upgrade --cask firefox
-elif [ "$SYSTEM" == "linux" ]; then
-    sudo apt install firefox
-fi
+sudo apt install firefox
